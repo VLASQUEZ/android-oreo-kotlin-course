f06b9e6d5a0673deece1f6f14b7c330463393a93
Andres Velasquez <VLASQUEZ53069@HOTMAIL.COM>
2018-05-01 11:08:28 -0500

    Creacion de clases con kotlin, constructores secundarios y propiedades

---
+++ Seccion_2_Kotlin_OOP/src/Dog.kt
@@ -0,0 +1,9 @@
+/**
+@autor Andrés Velasquez
+@since 1/05/18
+ **/
+class Dog(val name: String,var age : Int,val race: String){
+
+
+  constructor(name : String, race: String) : this(name,0,race)
+}
---
+++ Seccion_2_Kotlin_OOP/src/Main.kt
@@ -0,0 +1,9 @@
+/**
+@autor Andrés Velasquez
+@since 1/05/18
+ **/
+
+fun main(args: Array<String>){
+  val perrito = Dog("Apolo","Labrador")
+  print("${perrito.name} tiene ${perrito.age} años y es un ${perrito.race}")
+}
